30f1cdbb14b37956055d41a776877ff1c67e372b
zhang925 <zhang925518@163.com>
2018-08-01 17:02:06 +0800

    移动目录

--- src/com/zzy/webservice/CategoryRest.java
+++ src/com/zzy/webservice/CategoryRest.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**分类管理的RestController*/
 @RestController
-@RequestMapping(value = "/category")
+@RequestMapping(value = "/category/rest/")
 public class CategoryRest {
 
 }
