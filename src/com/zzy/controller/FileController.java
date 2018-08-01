a31722b2039c96142fe1b4c5f4525938cd7c8d05
zhang925 <zhang925518@163.com>
2018-08-01 17:01:10 +0800

    移动目录

--- src/com/zzy/controller/FileController.java
+++ src/com/zzy/controller/FileController.java
@@ -14,6 +14,7 @@ import java.io.FileOutputStream;
 
 
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -40,7 +41,8 @@ import org.apache.commons.fileupload.FileItemIterator;
 import org.apache.commons.fileupload.FileItemStream;  
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;  
 import org.apache.commons.fileupload.servlet.ServletFileUpload;  
-import org.apache.commons.fileupload.util.Streams;  
+import org.apache.commons.fileupload.util.Streams;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -66,6 +68,11 @@ import com.zzy.util.util_Json;
 import com.zzy.util.util_Md5;
 import com.zzy.util.file.util_down;
 import com.zzy.util.file.util_uploadForCommon;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
 /**
  * 文件控制层(FilesController)
  * @author zzy
@@ -179,15 +186,171 @@ public class FileController {
 		
 	}
 
-	
-	
-	
-	
-	
-	
 
-	
 
-	
+
+	/**
+	 *  上传,支持多文件
+	 *
+	 * <form id="uploadFileForm" action="#" enctype="multipart/form-data" >
+	 <input type="file" id="file" name="file" />
+	 </form>
+	 *   var formData = new FormData($("#faward" )[0]);
+	 $.ajax({
+	 url:"${path}/file/upload",
+	 type:"POST",
+	 cache:false,
+	 async:false,
+	 processData: false,
+	 contentType: false,
+	 data:formData,
+	 dataType:"json",
+	 success:function(data){},
+	 error:function(data){}
+	 });
+	 */
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public void upload(@RequestParam("file") CommonsMultipartFile files[], HttpServletRequest request, HttpServletResponse response){
+		String resMsg = "";
+		try {
+			if(files!=null && files.length>0){
+				for(CommonsMultipartFile file : files){
+					//String fileName = file.getOriginalFilename();
+					//String path="/Users/loukai/easylife/files/"+new Date().getTime()+file.getOriginalFilename();
+					//服务器的真是路径，发布项目的 根路径 F：aa/bb
+					String basePath = request.getSession().getServletContext().getRealPath("/");
+					//按照时间 建立一个文件 每天一个文件夹
+					SimpleDateFormat sd = new SimpleDateFormat("yyyyMMdd");//格式化 文件夹 名字
+					String timestr =  sd.format(new Date());
+					SimpleDateFormat sd2 = new SimpleDateFormat("yyyyMMddHHmmss");//格式化 文件 名字的前缀
+					//因为 Spring MVC 对静态 资源的控制原因，这里的file 上传到，"WEB-INF/skin/"下面
+					String originname = file.getOriginalFilename();//文件的原名字
+					String addPath="uploadfile/"+timestr+"/"+sd2.format(new Date())+originname;
+					String path = basePath + addPath;
+					File newFile=new File(path);
+					if(!newFile.exists()){
+						newFile.mkdirs();
+					}
+					file.transferTo(newFile);//通过CommonsMultipartFile的方法直接写文件
+					response.setCharacterEncoding("UTF8");
+					resMsg = addPath + ";" +resMsg;
+				}
+
+			}
+			if(resMsg.length()>1){//去掉最后一个分号
+				resMsg=resMsg.substring(0,resMsg.length()-1);
+			}
+			response.getWriter().write(resMsg);
+			response.getWriter().close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			resMsg = "error";
+		}
+	}
+
+
+	@RequestMapping(value = "down")
+	@ResponseBody
+	public void down(String fileid,HttpServletRequest request, HttpServletResponse response){
+		String src = filesService.getFilesModelByFID(fileid).getFilesrc();
+		downFileBySrc( request,  response,  src);
+	}
+
+	/**
+	 *  文件下载 根据文件地址 下载
+	 * @param request
+	 * @param response
+	 * @param src //文件路径
+	 */
+	public void  downFileBySrc(HttpServletRequest request, HttpServletResponse response, String src){
+		String downLoadPath = "";// 下载地址，是真实的 盘符 HttpServletResponse
+		// storeName 类似  skin/uploadfile/noticefile/20180604/20180604114710QQ截图.png
+		String basePath = request.getSession().getServletContext().getRealPath("/");// F:/platform/
+		downLoadPath =  basePath +"WEB-INF/" +src ;
+		String fileName = src.substring((src.lastIndexOf("/")+1),src.length());//获得 20180604114710QQ截图.png
+		String storeName =  fileName.substring(14,fileName.length());//下载后要显示的文件名字 QQ截图.png
+		try{
+			request.setCharacterEncoding("UTF-8");
+			BufferedInputStream bis = null;
+			BufferedOutputStream bos = null;
+
+			//获取项目根目录
+			//String ctxPath = request.getSession().getServletContext().getRealPath("");
+			//获取下载文件
+			//String downLoadPath = ctxPath+"/uploadFile/"+ storeName;
+			//这里暂时认为文件已经确定
+			//获取文件的长度
+
+			File file = new File(downLoadPath);
+			if (!file.exists()) {//文件不存在
+				try {
+					response.sendError(404, "服务器文件已经丢失，下载失败!");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				//return response;
+
+			}
+			long fileLength = file.length();
+			//设置文件输出类型
+			response.setContentType("application/octet-stream");
+			response.setHeader("Content-disposition", "attachment; filename="
+					+ new String(storeName.getBytes("utf-8"), "ISO8859-1"));
+			//设置输出长度
+			response.setHeader("Content-Length", String.valueOf(fileLength));
+			//获取输入流
+			bis = new BufferedInputStream(new FileInputStream(downLoadPath));
+			//输出流
+			bos = new BufferedOutputStream(response.getOutputStream());
+			byte[] buff = new byte[2048];
+			int bytesRead;
+			while (-1 != (bytesRead = bis.read(buff, 0, buff.length))) {
+				bos.write(buff, 0, bytesRead);
+			}
+			//关闭流
+			bis.close();
+			bos.close();
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		//return  response;
+	}
+
+
+	/**
+	 *
+	 * 根据文件路径删除文件
+	 * @param request
+	 * @param response
+	 * @param src
+	 */
+	@RequestMapping(value = "delete")
+	@ResponseBody
+	public String delete(HttpServletRequest request, HttpServletResponse response, String src){
+		response.setCharacterEncoding("utf8");
+		String basePath = request.getSession().getServletContext().getRealPath("/");
+		if(StringUtils.isEmpty(src)){
+			return "src  necessary !";
+		}
+		//本项目中，文件放到 WEB-INF skin 下
+		String fileName = basePath + "WEB-INF/"+ src ;
+		File file = new File(fileName);
+		if(!file.exists()){//文件不存在
+			return "file is not exists!";
+		}else{
+			// 如果文件路径所对应的文件存在，并且是一个文件，则直接删除
+			if (file.isFile()) {
+				if (file.delete()) {
+					return "success";
+				} else {
+					return "system exception !";
+				}
+			}else{
+				return " the deleting is not a file ! ";
+			}
+		}
+
+	}
 	
 }
---
+++ src/com/zzy/webservice/CategoryRest.java
@@ -0,0 +1,11 @@
+package com.zzy.webservice;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**分类管理的RestController*/
+@RestController
+@RequestMapping(value = "/category")
+public class CategoryRest {
+
+}
